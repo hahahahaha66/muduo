3b088fea903724378cc4787c54d544195557ced6
hahaha <a310608318@gmail.com>
2025-05-07 18:55:55 +0800

    5.7

---
+++ testserver/test1.cpp
@@ -0,0 +1,57 @@
+#include "../muduo/net/EventLoop.h"
+#include "../muduo/net/tcp/InetAddress.h"
+#include "../muduo/net//tcp/TcpServer.h"
+#include "../muduo/net/Channel.h"
+#include <functional>
+
+class EchoServer{
+public:
+    EchoServer(EventLoop* loop, const InetAddress& addr, const std::string& name)
+        : server_(loop, addr, name),
+          loop_(loop)
+    {
+        server_.setConnectionCallback(std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
+        server_.setMessageCallback(std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        server_.setThreadNum(3);
+    }
+
+    void start()
+    {
+        server_.start();
+    }
+
+private:
+    void onConnection(const TcpConnectionPtr& conn)
+    {
+        if (conn->connected())
+            LOG_INFO << "Connection UP : " << conn->peerAddress().toIpPort().c_str();
+        else
+            LOG_INFO << "Connection DOWN : " << conn->peerAddress().toIpPort().c_str();
+    }
+
+    void onMessage(const TcpConnectionPtr&conn, Buffer* buf, Timestamp time)
+    {
+        std::string msg = buf->retrieveAllAsString();
+        conn->send(msg);
+        conn->shutdown();
+    }
+
+    EventLoop* loop_;
+    TcpServer server_;
+
+};
+
+int main() {
+    EventLoop loop;
+
+    InetAddress addr(8080);
+
+    EchoServer server(&loop, addr, "EchoServer-01");
+
+    server.start();
+
+    loop.loop();
+
+    return 0;
+
+}
